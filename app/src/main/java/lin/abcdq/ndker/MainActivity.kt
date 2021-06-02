2c1bd106f669a43feacfcc42ae712f524f987576
wml <18981745898@163.com>
2021-06-02 16:20:22 +0800

    ...

--- app/src/main/java/lin/abcdq/ndker/MainActivity.kt
+++ app/src/main/java/lin/abcdq/ndker/MainActivity.kt
@@ -3,6 +3,8 @@ package lin.abcdq.ndker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//https://blog.csdn.net/afei__/article/details/81290711
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
