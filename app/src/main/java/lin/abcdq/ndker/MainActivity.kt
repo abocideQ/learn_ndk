13a1350a3f7f6632a1f939fafb6ad04b09343ab4
wml <18981745898@163.com>
2021-06-08 13:48:02 +0800

    triangle completed

--- app/src/main/java/lin/abcdq/ndker/MainActivity.kt
+++ app/src/main/java/lin/abcdq/ndker/MainActivity.kt
@@ -1,13 +1,22 @@
 package lin.abcdq.ndker
 
 import android.graphics.Color
-import android.graphics.RenderNode
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    private var mRectangleBuffer = floatArrayOf(
+        -0.5f, 0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+    )
     private var mTriangleBuffer = floatArrayOf(
         0.0f, 0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
@@ -19,14 +28,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mRender.setData(mTriangleBuffer, mTriangleBuffer.size, mColor)
+        mRender.setData(mRectangleBuffer, mRectangleBuffer.size, mColor)
+//        mRender.setData(mTriangleBuffer, mTriangleBuffer.size, mColor)
         val mGLSurfaceView = findViewById<GLSurfaceView>(R.id.gsv_content)
         mGLSurfaceView.setEGLContextClientVersion(3)
         mGLSurfaceView.setRenderer(mRender)
+        resize(mGLSurfaceView)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         mRender.onDestroy()
+    }
+
+    private fun resize(view: View) {
+        val size = resources.displayMetrics.widthPixels
+        val layoutParams = view.layoutParams as RelativeLayout.LayoutParams
+        layoutParams.height = size
+        view.layoutParams = layoutParams
     }
 }
